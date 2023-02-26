42785a4738a59f3ae9a480e0ab51337b6edfc49c
Ghost - Ben <Bcaltons5@gmail.com>
2023-02-26 05:05:46 -0600

    LibFetch actually works now

--- tools/make/libFetch.py
+++ tools/make/libFetch.py
@@ -36,10 +36,10 @@ class LibFetch:
         svn_cmd = self.svn_path or tools.cfg.make.SVN_CMD
         git_cmd = self.git_path or tools.cfg.make.GIT_CMD
 
-        if not tools.utils.verify_interface(svn_cmd):
+        if not tools.shared.interface.verify_interface(svn_cmd):
             raise EnvironmentError(f"{svn_cmd} not found or is not working correctly.")
 
-        if not tools.utils.verify_interface(git_cmd):
+        if not tools.shared.interface.verify_interface(git_cmd):
             raise EnvironmentError(f"{git_cmd} not found or is not working correctly.")
 
     def parse_yaml(self):
@@ -64,6 +64,12 @@ class LibFetch:
         os.chmod(path, stat.S_IWRITE)
         os.unlink(path)
 
+    def get_name_from_url_path(self, path: str) -> str:
+        return path.split("/")[-1]
+
+    def get_path_without_name(self, path: str) -> str:
+        return "\\".join(path.split("/")[:-1])
+
     def fetch_svn_repo(self, name: str, url: str):
         self.logger.info(f"Fetching {name}...")
         repo_path = os.path.join(self.checkout_path, name)
@@ -75,56 +81,105 @@ class LibFetch:
         subprocess.run(command, capture_output=True, shell=True, cwd=repo_path)
         self.logger.info(f"Successfully pulled {name}.")
 
-    def fetch_git_repo(self, final_name: str, url: str | dict[str, str]):
+    def fetch_repo(self, final_name: str, url: str | dict[str, str], repo_type: str):
         new_url = None
-        name = name.replace(".", "-")
+        branch = None
+        internal_dir_name = None
+        final_dir_name = self.get_name_from_url_path(final_name)
+        final_dir_path = self.get_path_without_name(final_name)
 
         if isinstance(url, dict):
             new_url = url["url"]
             branch = url["branch"]
-
-        self.logger.info(f"Fetching {name}...")
-        repo_path = os.path.join(self.checkout_path, name)
-
-        if not os.path.exists(self.checkout_path):
-            os.makedirs(self.checkout_path)
-
-        try:
-            command = f"{tools.cfg.make.GIT_CMD} clone "
-            if new_url:
-                command += f"{new_url} -b {branch} "
+            internal_dir_name = self.get_name_from_url_path(new_url)
+        else:
+            if repo_type == "git":
+                internal_dir_name = self.get_name_from_url_path(url)
             else:
-                command += url
-            subprocess.run(
-                command, capture_output=True, shell=True, cwd=os.path.dirname(repo_path)
-            )
+                internal_dir_name = final_dir_name
 
-            git_dir = f"{repo_path}/.git"
-            github_dir = f"{repo_path}/.github"
+        self.logger.info(f"Fetching {internal_dir_name}...")
+        lib_path = os.path.join(self.checkout_path, final_dir_path, internal_dir_name)
+        self.logger.debug(lib_path)
 
-            if os.path.exists(git_dir):
-                shutil.rmtree(git_dir, onerror=self.on_rm_error)
+        if not os.path.exists(lib_path):
+            os.makedirs(lib_path)
 
-            if os.path.exists(github_dir):
-                shutil.rmtree(github_dir, onerror=self.on_rm_error)
+        if repo_type == "git":
+            try:
+                command = f"{tools.cfg.make.GIT_CMD} clone "
+                if new_url:
+                    command += f"{new_url} -b {branch} "
+                elif isinstance(url, str):
+                    command += url
 
-        except Exception as f:
-            self.logger.error(f)
-            self.logger.error(f"Error fetching {name}...")
-            return False
-        self.logger.info(f"Successfully pulled {name}.")
+                subprocess.run(
+                    command,
+                    capture_output=True,
+                    shell=True,
+                    cwd=os.path.dirname(lib_path),
+                )
 
+                git_dir = f"{lib_path}/.git"
+                github_dir = f"{lib_path}/.github"
+
+                if os.path.exists(git_dir):
+                    self.logger.debug(
+                        f"Removing .git directory for {internal_dir_name}..."
+                    )
+                    shutil.rmtree(git_dir, onerror=self.on_rm_error)
+
+                if os.path.exists(github_dir):
+                    self.logger.debug(
+                        f"Removing .github directory for {internal_dir_name}..."
+                    )
+                    shutil.rmtree(github_dir, onerror=self.on_rm_error)
+
+                final_path = os.path.join(
+                    self.checkout_path, final_dir_path, final_dir_name
+                )
+                if final_path != lib_path:
+                    os.rename(lib_path, final_path)
+
+            except Exception as f:
+                self.logger.error(f"Error fetching {internal_dir_name}...")
+                self.logger.critical(f)
+                return False
+
+        elif repo_type == "svn":
+            try:
+                command = f"{tools.cfg.make.SVN_CMD} export {new_url or url} {lib_path} --force"
+
+                subprocess.run(
+                    command,
+                    capture_output=True,
+                    shell=True,
+                    cwd=os.path.dirname(lib_path),
+                )
+
+                final_path = os.path.join(
+                    self.checkout_path, final_dir_path, final_dir_name
+                )
+                if final_path != lib_path:
+                    os.rename(lib_path, final_path)
+
+            except Exception as f:
+                self.logger.error(f"Error fetching {internal_dir_name}...")
+                self.logger.critical(f)
+                return False
+
+        self.logger.info(f"Successfully pulled {internal_dir_name}.")
         return True
 
     def get_external_libs(self):
         if self.yaml_externals:
             for name, url in self.yaml_externals.items():
                 if "/trunk" in url:
-                    self.fetch_svn_repo(name, url)
+                    self.fetch_repo(name, url, "svn")
                 else:
-                    self.fetch_git_repo(name, url)
+                    self.fetch_repo(name, url, "git")
             self.move_lib_folders()
-            self.cleanup()
+            # self.cleanup()
             self.logger.info("Lib fetching complete!")
 
             return True
@@ -146,6 +201,7 @@ class LibFetch:
 
             # copy the folders to their respective destinations from the yaml file - 'move-folders'
             if os.path.exists(source):
+                self.logger.debug(source, destination)
                 shutil.copytree(
                     source, destination, ignore=ignore_pattern, dirs_exist_ok=True
                 )
