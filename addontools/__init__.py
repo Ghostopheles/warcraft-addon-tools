6a6816c300bc70b214f4644691c2bd259ede4c87
Ghostopheles <ghost@kivatech.io>
2024-01-29 17:11:22 -0600

    ???

--- addontools/__init__.py
+++ addontools/__init__.py
@@ -1,10 +1,10 @@
 """The so-called 'warcraft addon tools'. Use at your own risk."""
 
-import addontools.config as cfg
-import addontools.luac as luac
-import addontools.make as make
-import addontools.shared as shared
-import addontools.yamlTools as yaml
-import addontools.addon as addon
-import addontools.start as start
-import addontools.agent as agent
+from . import config as cfg
+from . import luac as luac
+from . import make as make
+from . import shared as shared
+from . import yamlTools as yaml
+from . import addon as addon
+from . import start as start
+from . import agent as agent
